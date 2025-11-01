c379d2c9169e6ff91af2b30b8637faf54c747cfe
mac_itthikon <zero-no-connect@hotmail.com>
2025-11-01 20:07:34 +0700

    add get itthikon

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
     res.send('Hello World!')
 })
 
+app.get('/itthikon',(req,res)=>{
+    res.send('Itthikon!')
+})
+
 app.listen(port,() =>{
     console.log(`Start port ${port}`)
 })
